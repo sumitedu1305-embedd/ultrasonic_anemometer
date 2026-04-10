3e7233c485b034cdf2cbd7cd3d643431982c3475
sumitedu1305-embedd <sumitedu1305@gmail.com>
2026-04-10 11:01:32 +0530

    new day

--- data_aquisation_python/wind_0804.py
+++ data_aquisation_python/wind_0804.py
@@ -3,6 +3,8 @@ import struct
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
+from scipy.signal import butter, filtfilt
+
 
 print("done second time")
 
@@ -14,6 +16,9 @@ HEADER_SOUTHOUT = 0xAA55
 HEADER_NORTHOUT = 0xBB55
 
 PAYLOAD_SAMPLES = 500
+
+FS = 1e6
+
 # ------------------------
 
 ser = serial.Serial(PORT, BAUD, timeout=1)
@@ -51,32 +56,57 @@ def read_frame():
     return np.array(samples1), np.array(samples2)
 
 # -------- Plot Setup --------
-fig, axs = plt.subplots(2, 1, figsize=(8, 6), sharex=True)
+fig, axs = plt.subplots(4, 1, figsize=(8, 6), sharex=True)
 
 x = np.arange(PAYLOAD_SAMPLES)
 
 line1, = axs[0].plot(x, np.zeros(PAYLOAD_SAMPLES))
 line2, = axs[1].plot(x, np.zeros(PAYLOAD_SAMPLES))
+line3, = axs[2].plot(x, np.zeros(PAYLOAD_SAMPLES))
+line4, = axs[3].plot(x, np.zeros(PAYLOAD_SAMPLES))
 
-axs[0].set_title("Southout Raw Signal")
-axs[1].set_title("Northout Raw Signal")
+axs[0].set_ylim(-150, 150)
+axs[1].set_ylim(-150, 150)
+axs[2].set_ylim(-150, 150)
+axs[3].set_ylim(-150, 150)
 
-axs[0].set_ylim(1950, 2250)
-axs[1].set_ylim(1950, 2250)
+def bandpass_filter(data, lowcut, highcut, fs, order=4):
+    nyquist = 0.5 * fs
+    low = lowcut / nyquist
+    high = highcut / nyquist
+    
+    b, a = butter(order, [low, high], btype='band')
+    filtered = filtfilt(b, a, data)  # zero-phase filtering
+    
+    return filtered
 
 # -------- Update Loop --------
 def update(frame):
     samples1, samples2 = read_frame()
 
     samples1 = samples1 - np.mean(samples1)
-    samples2 = samples2 - np.mean(samples1)
+    samples2 = samples2 - np.mean(samples2)
+
+    window1 = np.hanning(len(samples1))
+    window2 = np.hanning(len(samples2))
+    samples1_hanning = samples1 * window1
+    samples2_hanning = samples2 * window2
+
+    samples1_filtered = bandpass_filter(samples1_hanning,30e3,50e3,FS)
+    samples2_filtered = bandpass_filter(samples2_hanning,30e3,50e3,FS)
 
     line1.set_ydata(samples1)
     line2.set_ydata(samples2)
+    line3.set_ydata(samples1_filtered)
+    line4.set_ydata(samples2_filtered)
 
     return line1, line2
 
-# -------- Run --------
-ani = FuncAnimation(fig, update, interval=50)
-plt.tight_layout()
-plt.show()
+
+try:
+    # -------- Run --------
+    ani = FuncAnimation(fig, update, interval=50)
+    plt.tight_layout()
+    plt.show()
+except KeyboardInterrupt:
+    plt.close('all')
