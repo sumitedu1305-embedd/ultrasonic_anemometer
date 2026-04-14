f1ca716ebb281cd94b75a1f1f8bb704220ff68c8
sumitedu1305-embedd <sumitedu1305@gmail.com>
2026-04-14 15:37:20 +0530

    firmware tweaked (delay buffer size changed for similar behavior)

--- data_aquisation_python/wind_1304.py
+++ data_aquisation_python/wind_1304.py
@@ -5,8 +5,6 @@ from scipy.signal import butter, filtfilt, correlate
 from collections import deque
 import threading, queue, os, sys
 
-#new for relfector based config
-
 # -------- CONFIG --------
 PORT = "COM6"
 BAUD = 921600
